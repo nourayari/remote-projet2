2a9f7d8f45f53db7d14125958c37deb07665593f
Nour Elhouda AYARI <nourelhouda.ayari@actia-engineering.tn>
2021-07-05 14:15:04 +0200

    Test rng.py

--- rng/rng.py
+++ rng/rng.py
@@ -5,3 +5,6 @@ import time
 app = Flask(__name__)
 hostname = socket.gethostname()
 urandom = os.open("/dev/urandom", os.O_RDONLY)
+@app.route("/")
+def index():
+return "RNG running on {}\n".format(hostname)
