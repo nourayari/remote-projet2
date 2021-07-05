db3e247fa9f4b33a2b5880306e570de4945045bf
Nour Elhouda AYARI <nourelhouda.ayari@actia-engineering.tn>
2021-07-05 14:16:48 +0200

    génération aléatoire d’octets

--- rng/rng.py
+++ rng/rng.py
@@ -5,6 +5,14 @@ import time
 app = Flask(__name__)
 hostname = socket.gethostname()
 urandom = os.open("/dev/urandom", os.O_RDONLY)
+
 @app.route("/")
 def index():
-return "RNG running on {}\n".format(hostname)
+	return "RNG running on {}\n".format(hostname)
+
+@app.route("/<int:how_many_bytes>")
+def rng(how_many_bytes):
+	time.sleep(0.1)
+	return Response(
+		os.read(urandom, how_many_bytes),
+		content_type="application/octet-stream")
